1fdc5a38753a50a5662137cef4eb0196c73df34f
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-16 09:02:03 -0300

    Padronizando resultados de handlers com CommandResult

---
+++ Todo.Domain/Commands/Contracts/CommandResult.cs
@@ -0,0 +1,27 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands.Contracts;
+
+public class CommandResult : ICommandResult
+{
+    #region Constructors
+
+    public CommandResult() { }
+    
+    public CommandResult(bool success, string message, object data)
+    {
+        Success = success;
+        Message = message;
+        Data = data;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public Object Data { get; set; }
+
+    #endregion
+}
