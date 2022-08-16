3a9398ce9f4d1ce0b4d62d4fd82a52d7198ee125
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-15 22:19:53 -0300

    BaseEntity

---
+++ Todo.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,28 @@
+﻿namespace Todo.Domain.Entities;
+
+public abstract class BaseEntity : IEquatable<BaseEntity>
+{
+    #region Constructor
+
+    private BaseEntity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    #endregion
+
+    #region Properties
+
+    private Guid Id { get; }
+
+    #endregion
+
+    #region Methods
+
+    public bool Equals(BaseEntity? other)
+    {
+        return Id == other?.Id;
+    }
+
+    #endregion
+}
