f3e59c3ee54b1a303b3ca524cceeecc578672506
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-16 09:13:18 -0300

    Implementação comando MarkTodoAsUndoneCommand

---
+++ Todo.Domain/Commands/MarkTodoAsUndoneCommand.cs
@@ -0,0 +1,43 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands;
+
+public class MarkTodoAsUndoneCommand : ICommand
+{
+    #region Contructors
+
+    public MarkTodoAsUndoneCommand() { }
+
+    public MarkTodoAsUndoneCommand(Guid id, string user)
+    {
+        Id = id;
+        User = user;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public Guid Id { get; set; }
+    public string User { get; set; }
+
+    #endregion
+
+    #region Methods
+
+    public bool Validate(out List<string> validationsErrors)
+    {
+        bool valid = true;
+        validationsErrors = new List<string>();
+
+        if (User.Length < 6)
+        {
+            validationsErrors.Add("O tamanho do usuário deve ser maior que 6 carácter");
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    #endregion
+}
