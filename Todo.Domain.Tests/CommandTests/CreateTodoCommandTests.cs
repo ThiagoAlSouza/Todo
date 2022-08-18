0220bf549b080ce879a3d1f23283e992f803f1a7
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-17 21:50:24 -0300

    Testes iniciais com o comando CreateTodoCommandTests

---
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Todo.Domain.Commands;
+
+namespace Todo.Domain.Tests.CommandTests;
+
+public class CreateTodoCommandTests
+{
+    [Fact]
+    public void WhenACommandIsValid()
+    {
+        var command = new CreateTodoCommand("Estudar.Net 7", "Thiago", DateTime.Now);
+        var errors = new List<string>();
+
+        Assert.True(command.Validate(out errors));
+    }
+
+    [Fact]
+    public void WhenACommandIsInvalid()
+    {
+        var command = new CreateTodoCommand("Ab", "Tiago", DateTime.Now);
+        var errors = new List<string>();
+
+        Assert.True(!command.Validate(out errors));
+    }
+}
