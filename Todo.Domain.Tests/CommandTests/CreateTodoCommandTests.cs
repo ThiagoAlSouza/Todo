992000b9e564aea73667c948c492d6bff3f79419
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-17 21:56:06 -0300

    Ajustes CreateTodoCommandTests

--- Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -4,21 +4,37 @@ namespace Todo.Domain.Tests.CommandTests;
 
 public class CreateTodoCommandTests
 {
+    #region Privates
+
+    private readonly CreateTodoCommand _invalidCommand;
+    private readonly CreateTodoCommand _validCommand;
+    private List<string> errors = new();
+
+    #endregion
+
+    #region Constructors
+
+    public CreateTodoCommandTests()
+    {
+        _invalidCommand = new CreateTodoCommand("Ab", "Tiago", DateTime.Now);
+        _validCommand = new CreateTodoCommand("Estudar.Net 7", "Thiago", DateTime.Now);
+    }
+
+    #endregion
+
+    #region Methods
+
     [Fact]
     public void WhenACommandIsValid()
     {
-        var command = new CreateTodoCommand("Estudar.Net 7", "Thiago", DateTime.Now);
-        var errors = new List<string>();
-
-        Assert.True(command.Validate(out errors));
+        Assert.True(_validCommand.Validate(out errors));
     }
 
     [Fact]
     public void WhenACommandIsInvalid()
     {
-        var command = new CreateTodoCommand("Ab", "Tiago", DateTime.Now);
-        var errors = new List<string>();
-
-        Assert.True(!command.Validate(out errors));
+        Assert.True(!_invalidCommand.Validate(out errors));
     }
+
+    #endregion
 }
