a85d87045fab148023ebcf61591fc64b6bfbcc73
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-19 09:06:04 -0300

    Criação DataContext

---
+++ Todo.Domain.Infra/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Infra.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DbContext> options) : base(options) { }
+
+    public DbSet<TodoItem> Todos { get; set; }
+}
