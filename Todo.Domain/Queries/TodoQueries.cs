054d6f862bda5a0d49f4fbcbe58a397a0360abb3
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-20 19:35:05 -0300

    Implementação TodoQueries

---
+++ Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries;
+
+public static class TodoQueries
+{
+    #region Methods
+
+    public static Expression<Func<TodoItem, bool>> GetAll(string user)
+    {
+        return x => x.User == user;
+    }
+
+    public static Expression<Func<TodoItem, bool>> GetAllDone(string user)
+    {
+        return x => x.User == user && x.Done;
+    }
+
+    #endregion
+}
