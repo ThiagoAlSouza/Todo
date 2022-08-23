e82cdce10d717832b81d0dae72c0a9b59fb2e523
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-22 23:16:56 -0300

    Implementação TodoController Post

---
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+using Todo.Domain.Infra.Data;
+
+namespace Todo.Domain.Api.Controllers;
+
+[ApiController]
+[Route("v1")]
+public class TodoController : ControllerBase
+{
+    #region Methods
+
+    [HttpPost("todos")]
+    public IActionResult Post([FromBody] CreateTodoCommand todo
+    , [FromServices] TodoItemHandler handler)
+    {
+        try
+        {
+            handler.Handle(todo);
+            return Ok(new CommandResult(true, "Registro salvo com sucesso.", todo));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new CommandResult(false, e.Message, todo));
+        }
+    }
+
+    #endregion
+}
